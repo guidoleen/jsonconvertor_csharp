2b3a624bd5402ffbba6478b82d676a8246b47cce
Guido Leen <guido_leen@transfer-solutions.local>
2018-12-18 14:51:40 +0100

    eerste keer

---
+++ ObjectToJson.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JsonObjectConvertor
+{
+    public class ObjectToJson: IObjectJson
+    {
+        private Object obj;
+
+        public ObjectToJson(Object _obj)
+        {
+            this.obj = _obj;
+        }
+
+        public String setToJsonString()
+        {
+            string output = JsonConvert.SerializeObject(this.obj, Formatting.Indented).ToString();
+            return output;
+        }
+    }
+}
